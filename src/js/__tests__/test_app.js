121e5f2bdb1394792d5c11e41999cd1fa0b8313a
Sidorov Maksim <maxx11167@gmail.com>
2022-12-17 18:29:39 +0300

    test is ready

---
+++ src/js/__tests__/test_app.js
@@ -0,0 +1,37 @@
+import getUpgrateHealthyStatus from '../app.js';
+
+test('healthyStatusByToEqual', () => {
+  const result = getUpgrateHealthyStatus([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  const expct = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).toEqual(expct);
+});
+
+test('healthyStatusByToBe', () => {
+  const result = getUpgrateHealthyStatus([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  const expct = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).toBe(expct);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-console.log('It\'s working!');
+export default function getUpgrateHealthyStatus(array) {
+  return array.sort((a, b) => {
+    if (a.health < b.health) {
+      return 1;
+    }
+    if (a.health > b.health) {
+      return -1;
+    }
+
+    return 0;
+  });
+}
