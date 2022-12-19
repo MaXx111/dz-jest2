7b31d510dcd8a461595614eecf0af87f3163d60c
Sidorov Maksim <maxx11167@gmail.com>
2022-12-19 14:23:46 +0300

    Add appveyor.yml

--- src/js/__tests__/test_app.js
+++ src/js/__tests__/test_app.js
@@ -35,3 +35,21 @@ test('healthyStatusByToBe', () => {
 
   expect(result).toBe(expct);
 });
+
+test('healthyStatusByToBe', () => {
+  const result = getUpgrateHealthyStatus([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  const expct = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 50 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).not.toBe(expct);
+});
